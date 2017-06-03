696bccde1ce595c67c5e65026ca366e6f1fd5a0d
Esben S. Nielsen <storpipfugl@gmail.com>
2017-06-03 13:32:52 +0200

    Release v1.2.2

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ class build_ext_subclass(build_ext):
         
 setup(
     name='pykdtree',
-    version='1.2.1',
+    version='1.2.2',
     description='Fast kd-tree implementation with OpenMP-enabled queries',
     author='Esben S. Nielsen',
     author_email='storpipfugl@gmail.com',
