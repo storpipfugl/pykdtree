cbae790e714bb3a4b2501d1881043a81f06eff95
David Hoese <david.hoese@ssec.wisc.edu>
2024-01-17 20:42:28 -0600

    Add OpenMP warning when extension fails to import

---
+++ pykdtree/__init__.py
@@ -0,0 +1,14 @@
+"""Simple wrapper around importing pykdtree for OpenMP use or not."""
+
+try:
+    from . import kdtree
+except ImportError:
+    import warnings
+    warnings.warn(
+        "Pykdtree failed to import its C extension. This usually means it "
+        "was built with OpenMP (C-level parallelization library) support but "
+        "could not find it on your system. To enable better performance "
+        "OpenMP must be installed (ex. ``brew install omp`` on Mac with "
+        "HomeBrew). Otherwise, try installing Pykdtree from source (ex. "
+        "``pip install --no-binary pykdtree --force-install pykdtree``)."
+    )
