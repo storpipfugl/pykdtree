d870fc53f0127ef8c3cfe96ded2ad1760e4bf3e9
Kevin Welsh <kevin.welsh@verdantevolution.com>
2025-06-02 13:09:08 -0400

    add test case for keeping stubs in sync

---
+++ pykdtree/test_stub.py
@@ -0,0 +1,22 @@
+from mypy import stubtest
+from contextlib import redirect_stdout
+from io import StringIO
+
+
+def test_mypy():
+    """
+    Run mypy stub tests for pykdtree.
+    This function checks for:
+        - Type consistency in the stubs vs the definitions
+        - Function / property signatures
+        - Missing functions or properties in the stubs
+    """
+
+    out = StringIO()
+    with redirect_stdout(out):
+        code = stubtest.test_stubs(stubtest.parse_options(["pykdtree.kdtree"]))
+
+    assert code == 0, "Mypy stub test failed:\n" + out.getvalue()
+
+
+test_mypy()
